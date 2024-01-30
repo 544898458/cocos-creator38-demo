704d4f75787a5e8e62b9fb8289b8593ae79fa5f7
spjs <20024675@qq.com>
2024-01-30 11:35:57 +0800

    放地面和人，显示不出来

--- assets/UiLogin.ts
+++ assets/UiLogin.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component,EditBox,Button } from 'cc';
+import {Node,resources,Prefab,instantiate, _decorator, Component,EditBox,Button,Vec3 } from 'cc';
 import msgpack from "msgpack-lite/dist/msgpack.min.js";
 
 const { ccclass, property } = _decorator;
@@ -48,18 +48,33 @@ export class UiLogin extends Component {
               websocket.send(encoded);
            }
 
+           let plane = this.node.parent.getChildByName("Plane");
           //接收到消息的回调方法
            websocket.onmessage = function (event: MessageEvent) {
                let data = event.data as ArrayBuffer
                console.log("收到数据：", data, data.byteLength);
                const arr = msgpack.decode(new Uint8Array(data))
+               let id = arr[0];
+               let posX = arr[1];
                console.log(arr);
+
+               
+               resources.load("altman-blue", Prefab, (err, prefab) => 
+                {
+                    console.log('resources.load callback:',err,prefab);
+                    const newNode = instantiate(prefab);
+                    plane.addChild(newNode);
+                    newNode.position = Vec3(posX,0,0);
+                    console.log('resources.load newNode',newNode);
+                    // this.view=newNode;
+                });
           }
 
            //连接关闭的回调方法
-           websocket.onclose = function () {
-              console.log("WebSocket连接关闭");
-           }
+           websocket.onclose = function (e) {
+            console.log('websocket 断开: ' + e.code + ' ' + e.reason + ' ' + e.wasClean)
+            console.log(e)
+          }
 
            //监听窗口关闭事件，当窗口关闭时，主动去关闭websocket连接，防止连接还没断开就关闭窗口，server端会抛异常。
            window.onbeforeunload = function () {
