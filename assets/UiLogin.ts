590e587f7df9701e10b29e25ff67adef89d8b4c1
格里锋 <544898458@qq.com>
2024-02-17 15:29:44 +0800

    各个连接单独控制自己的单位

--- assets/UiLogin.ts
+++ assets/UiLogin.ts
@@ -1,49 +1,48 @@
-import {Node,resources,Prefab,instantiate, _decorator, Component,EditBox,Button,Vec3,NodeEventType,EventMouse,geometry,PhysicsSystem,Camera  } from 'cc';
+import { Node, resources, Prefab, instantiate, _decorator, Component, EditBox, Button, Vec3, NodeEventType, EventMouse, geometry, PhysicsSystem, Camera } from 'cc';
 import msgpack from "msgpack-lite/dist/msgpack.min.js";
 
 const { ccclass, property } = _decorator;
-class ClientEntityComponent extends Component{
-    view :Node
+class ClientEntityComponent extends Component {
+    view: Node
 }
-enum MsgId
-{
-	Login,
-	Move,
+enum MsgId {
+    Login,
+    Move,
+    LoginRet,
+    NotifyPos,
 };
 
 @ccclass('UiLogin')
 export class UiLogin extends Component {
-    entities : { [key: number]: ClientEntityComponent; } ={};
-    websocket : WebSocket
+    entities: { [key: number]: ClientEntityComponent; } = {};
+    websocket: WebSocket
     start() {
         this.node.on(NodeEventType.MOUSE_DOWN, (event: EventMouse) => {
-            console.log('MOUSE_DOWN',event)
+            console.log('MOUSE_DOWN', event)
             var uiPos = event.getLocation();
             var ray = new geometry.Ray();
             // const camera = cc.find("Camera",this.node).getComponent(Camera);
-            const camera = cc.find("Main Camera",this.node.parent).getComponent(Camera);
+            const camera = cc.find("Main Camera", this.node.parent).getComponent(Camera);
             camera.screenPointToRay(uiPos.x, uiPos.y, ray);
             if (PhysicsSystem.instance.raycast(ray)) {
                 const raycastResults = PhysicsSystem.instance.raycastResults;
                 for (let i = 0; i < raycastResults.length; i++) {
                     const item = raycastResults[i];
-                    console.log('射线碰撞',i,item.collider.node.name,item.hitPoint);
-                    if (item.collider.node.name == "Plane") 
-                    {
+                    console.log('射线碰撞', i, item.collider.node.name, item.hitPoint);
+                    if (item.collider.node.name == "Plane") {
                         const object = //item.hitPoint;
-                        [
-                            MsgId.Move,
-                            item.hitPoint.x,
-                            item.hitPoint.y,
-                            item.hitPoint.z
-                        ];
-                          
-                          const encoded: Uint8Array = msgpack.encode(object);
-                          if(this.websocket!=undefined)
-                          {
+                            [
+                                MsgId.Move,
+                                item.hitPoint.x,
+                                item.hitPoint.y,
+                                item.hitPoint.z
+                            ];
+
+                        const encoded: Uint8Array = msgpack.encode(object);
+                        if (this.websocket != undefined) {
                             console.log(encoded);
                             this.websocket.send(encoded);
-                          }
+                        }
                     }
                 }
             } else {
@@ -53,19 +52,19 @@ export class UiLogin extends Component {
     }
 
     update(deltaTime: number) {
-        
+
     }
 
     onClickLogin(event: Event, customEventData: string) {
-    // 这里 event 是一个 Touch Event 对象，你可以通过 event.target 取到事件的发送节点
+        // 这里 event 是一个 Touch Event 对象，你可以通过 event.target 取到事件的发送节点
         const node = event.target as Node;
         const button = node.getComponent(Button);
-        const editNode = cc.find("Name",this.node) as Node;
-        console.log(button); 
-        console.log(editNode); 
+        const editNode = cc.find("Name", this.node) as Node;
+        console.log(button);
+        console.log(editNode);
 
         const editBox = editNode.getComponent(EditBox);
-        console.log(editBox.string); 
+        console.log(editBox.string);
 
         //this.websocket = new WebSocket("ws://192.168.31.138:12345/");
         this.websocket = new WebSocket("ws://10.0.35.76:12345/");
@@ -74,67 +73,74 @@ export class UiLogin extends Component {
         console.log(this.websocket)
         var gameclient = this
 
-          //连接发生错误的回调方法
-          this.websocket.onerror = function () {
-               console.log("WebSocket连接发生错误");
-          };
+        //连接发生错误的回调方法
+        this.websocket.onerror = function () {
+            console.log("WebSocket连接发生错误");
+        };
 
-           //连接成功建立的回调方法
-           this.websocket.onopen = (event: Event)=> {
-               console.log("WebSocket连接成功");
-               const object = [
+        //连接成功建立的回调方法
+        this.websocket.onopen = (event: Event) => {
+            console.log("WebSocket连接成功");
+            const object = [
                 MsgId.Login,
                 editBox.string,
                 'Hello, world!pwd',
-               ];
-              
-              const encoded: Uint8Array = msgpack.encode(object);
-              console.log(encoded);
-              this.websocket.send(encoded);
-           }
+            ];
 
-           let plane = this.node.parent.getChildByName("Plane");
-           let entites = this.entities;
-          //接收到消息的回调方法
-          this.websocket.onmessage = function (event: MessageEvent) {
-               let data = event.data as ArrayBuffer
-               console.log("收到数据：", data, data.byteLength);
-               const arr = msgpack.decode(new Uint8Array(data))
-               let id = arr[0];
-               let posX = arr[1];
-               console.log(arr);
+            const encoded: Uint8Array = msgpack.encode(object);
+            console.log(encoded);
+            this.websocket.send(encoded);
+        }
 
-               let old = entites[id]
-               if( old==undefined)
-               {
-                    entites[id]=new ClientEntityComponent();
-                    resources.load("altman-blue", Prefab, (err, prefab) => 
-                    {
-                        console.log('resources.load callback:',err,prefab);
-                        const newNode = instantiate(prefab);
-                        plane.addChild(newNode);
-                        newNode.position =new Vec3(posX,0,0);
-                        console.log('resources.load newNode',newNode);
-                        entites[id].view=newNode;
-                    });
-                }
-                else
-                {
-                    if( old != undefined )
-                        old.view.position=new Vec3(posX,0,0);
-                }
-          }
+        let plane = this.node.parent.getChildByName("Plane");
+        let entites = this.entities;
+        //接收到消息的回调方法
+        this.websocket.onmessage = function (event: MessageEvent) {
+            let data = event.data as ArrayBuffer
+            console.log("收到数据：", data, data.byteLength)
+            const arr = msgpack.decode(new Uint8Array(data))
+            console.log("msgpack.decode结果：", data, data.byteLength)
+            let msgId = arr[0] as MsgId
+            switch (msgId)
+            {
+                case MsgId.LoginRet:
+                    console.log('登录成功');
+                    break;
+                case MsgId.NotifyPos:
+                    let id = arr[1]
+                    let posX = arr[2]
+                    console.log(arr)
 
-           //连接关闭的回调方法
-           this.websocket.onclose = function (e) {
+                    let old = entites[id]
+                    if (old == undefined) {
+                        entites[id] = new ClientEntityComponent();
+                        resources.load("altman-blue", Prefab, (err, prefab) => {
+                            console.log('resources.load callback:', err, prefab);
+                            const newNode = instantiate(prefab);
+                            plane.addChild(newNode);
+                            newNode.position = new Vec3(posX, 0, 0);
+                            console.log('resources.load newNode', newNode);
+                            entites[id].view = newNode;
+                        });
+                    }
+                    else {
+                        if (old != undefined && old.view != undefined)
+                            old.view.position = new Vec3(posX, 0, 0);
+                    }
+                    break;
+            }
+        }
+
+        //连接关闭的回调方法
+        this.websocket.onclose = function (e) {
             console.log('websocket 断开: ' + e.code + ' ' + e.reason + ' ' + e.wasClean)
             console.log(e)
-          }
+        }
 
-           //监听窗口关闭事件，当窗口关闭时，主动去关闭websocket连接，防止连接还没断开就关闭窗口，server端会抛异常。
-           window.onbeforeunload = function () {
-               console.log("onbeforeunload");
-           }
+        //监听窗口关闭事件，当窗口关闭时，主动去关闭websocket连接，防止连接还没断开就关闭窗口，server端会抛异常。
+        window.onbeforeunload = function () {
+            console.log("onbeforeunload");
+        }
     }
 
 }
