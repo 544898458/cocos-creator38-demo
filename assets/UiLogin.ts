ec1f47f8b9f42f361ce8da624906365310908011
spjs <20024675@qq.com>
2024-08-04 02:39:58 +0800

    阵亡后3秒消失，但是界面HUD没消失

--- assets/UiLogin.ts
+++ assets/UiLogin.ts
@@ -1,8 +1,8 @@
-import { Node, resources, Prefab, instantiate, _decorator, Component, EditBox, Button, Vec3, NodeEventType, EventMouse, geometry, PhysicsSystem, Camera, SkeletalAnimation, Label, utils, AnimationClip } from 'cc';
-import msgpack from "msgpack-lite/dist/msgpack.min.js";
-import { HeadScale } from './head-scale';
+import { Node, resources, Prefab, instantiate, _decorator, Component, EditBox, Button, Vec3, NodeEventType, EventMouse, geometry, PhysicsSystem, Camera, SkeletalAnimation, Label, utils, AnimationClip } from 'cc'
+import msgpack from "msgpack-lite/dist/msgpack.min.js"
+import { HeadScale } from './head-scale'
 
-const { ccclass, property } = _decorator;
+const { ccclass, property } = _decorator
 class ClientEntityComponent{
     view: Node
     nodeName:Node
@@ -17,55 +17,56 @@ enum MsgId {
     Invalid_0,
     Login,
     Move,
-    LoginRet,
+    AddRoleRet,
     NotifyPos,
     ChangeSkeleAnim,
     Say,
     SelectRoles,
     AddRole,
-};
+    DelRoleRet
+}
 
 @ccclass('UiLogin')
 export class UiLogin extends Component {
-    entities: Map<number,ClientEntityComponent> = new Map<number,ClientEntityComponent>;
-    entityId = new Map<string, number>;//uuid=>服务器ID
+    entities: Map<number,ClientEntityComponent> = new Map<number,ClientEntityComponent>
+    entityId = new Map<string, number>//uuid=>服务器ID
     websocket: WebSocket
     targetFlag: Node//走路走向的目标点
     lableMessage: Label
     start() {
-        this.targetFlag = utils.find("Roles/TargetFlag", this.node.parent);
-        this.lableMessage = utils.find("Canvas/Message", this.node.parent).getComponent(Label);
-        let targetFlag = this.targetFlag;
+        this.targetFlag = utils.find("Roles/TargetFlag", this.node.parent)
+        this.lableMessage = utils.find("Canvas/Message", this.node.parent).getComponent(Label)
+        let targetFlag = this.targetFlag
         this.node.on(NodeEventType.MOUSE_DOWN, (event: EventMouse) => {
             console.log('MOUSE_DOWN', event)
-            var uiPos = event.getLocation();
-            var ray = new geometry.Ray();
-            // const camera = cc.find("Camera",this.node).getComponent(Camera);
-            const camera = utils.find("Main Camera", this.node.parent).getComponent(Camera);
-            camera.screenPointToRay(uiPos.x, uiPos.y, ray);
+            var uiPos = event.getLocation()
+            var ray = new geometry.Ray()
+            // const camera = cc.find("Camera",this.node).getComponent(Camera)
+            const camera = utils.find("Main Camera", this.node.parent).getComponent(Camera)
+            camera.screenPointToRay(uiPos.x, uiPos.y, ray)
             if (!PhysicsSystem.instance.raycastClosest(ray)) 
             {
-                console.log('raycast does not hit the target node !');
-                return false;
+                console.log('raycast does not hit the target node !')
+                return false
             }
 
-            const raycastResults = PhysicsSystem.instance.raycastClosestResult;
+            const raycastResults = PhysicsSystem.instance.raycastClosestResult
             
-            const item = raycastResults//[i];
-            console.log('射线碰撞', item.collider.node.name, item.hitPoint);
+            const item = raycastResults//[i]
+            console.log('射线碰撞', item.collider.node.name, item.hitPoint)
             if (item.collider.node.name == "Plane") 
             {
-                targetFlag.position = item.hitPoint;
+                targetFlag.position = item.hitPoint
             
-                const object = //item.hitPoint;
+                const object = //item.hitPoint
                     [
                         MsgId.Move,
                         item.hitPoint.x,
                         // item.hitPoint.y,
                         item.hitPoint.z
-                    ];
+                    ]
 
-                const encoded: Uint8Array = msgpack.encode(object);
+                const encoded: Uint8Array = msgpack.encode(object)
                 if (this.websocket != undefined) {
                     console.log('send',encoded)
                     this.websocket.send(encoded)
@@ -83,9 +84,9 @@ export class UiLogin extends Component {
                     [
                         MsgId.SelectRoles,
                         [id]//虽然是整数，但是也强制转成FLOAT64发出去了/*  */
-                    ];
+                    ]
 
-                const encoded: Uint8Array = msgpack.encode(object);
+                const encoded: Uint8Array = msgpack.encode(object)
                 if (this.websocket != undefined) {
                     console.log('send',encoded)
                     this.websocket.send(encoded)
@@ -95,41 +96,41 @@ export class UiLogin extends Component {
                     let nodEffect = clientEntityComponent.view.getChildByName(prefabName)
                     console.log('准备删除',nodEffect)
                     clientEntityComponent.view.removeChild(nodEffect)
-                });
+                })
                 resources.load(prefabName, Prefab, (err, prefab) => {
-                    console.log('resources.load callback:', err, prefab);
-                    const newNode = instantiate(prefab);
+                    console.log('resources.load callback:', err, prefab)
+                    const newNode = instantiate(prefab)
                     newNode.name = prefabName
-                    this.entities.get(id).view.addChild(newNode);
-                });
+                    this.entities.get(id).view.addChild(newNode)
+                })
             }
         
         
             return false
-        }, this);
+        }, this)
     }
 
     update(deltaTime: number) {
 
     }
     onClickAddRole(event: Event, customEventData: string):void {
-        const encoded: Uint8Array = msgpack.encode([MsgId.AddRole]);
-        console.log(encoded);
-        this.websocket.send(encoded);
+        const encoded: Uint8Array = msgpack.encode([MsgId.AddRole])
+        console.log(encoded)
+        this.websocket.send(encoded)
     }
     onClickLogin(event: Event, customEventData: string) {
         // 这里 event 是一个 Touch Event 对象，你可以通过 event.target 取到事件的发送节点
-        const node = event.target as Node;
-        const button = node.getComponent(Button);
-        const editNode = utils.find("Name", this.node) as Node;
-        console.log(button);
-        console.log(editNode);
+        const node = event.target as Node
+        const button = node.getComponent(Button)
+        const editNode = utils.find("Name", this.node) as Node
+        console.log(button)
+        console.log(editNode)
 
-        const editBox = editNode.getComponent(EditBox);
-        console.log(editBox.string);
+        const editBox = editNode.getComponent(EditBox)
+        console.log(editBox.string)
 
-        this.websocket = new WebSocket("ws://192.168.31.194:12345/");
-        // this.websocket = new WebSocket("ws://10.0.35.76:12345/");
+        this.websocket = new WebSocket("ws://192.168.31.194:12345/")
+        // this.websocket = new WebSocket("ws://10.0.35.76:12345/")
 
         this.websocket.binaryType = 'arraybuffer'
         console.log(this.websocket)
@@ -137,26 +138,26 @@ export class UiLogin extends Component {
 
         //连接发生错误的回调方法
         this.websocket.onerror = function () {
-            console.log("WebSocket连接发生错误");
-        };
+            console.log("WebSocket连接发生错误")
+        }
 
         //连接成功建立的回调方法
         this.websocket.onopen = (event: Event) => {
-            console.log("WebSocket连接成功");
+            console.log("WebSocket连接成功")
             const object = [
                 MsgId.Login,
                 editBox.string,
                 'Hello, world!pwd',
-            ];
+            ]
 
-            const encoded: Uint8Array = msgpack.encode(object);
-            console.log(encoded);
-            this.websocket.send(encoded);
+            const encoded: Uint8Array = msgpack.encode(object)
+            console.log(encoded)
+            this.websocket.send(encoded)
         }
 
-        let roles = this.node.parent.getChildByName("Roles");
-        let entities = this.entities;
-        let entityId = this.entityId;
+        let roles = this.node.parent.getChildByName("Roles")
+        let entities = this.entities
+        let entityId = this.entityId
         let lableMessage = this.lableMessage
         //接收到消息的回调方法
         this.websocket.onmessage = function (event: MessageEvent) {
@@ -168,51 +169,52 @@ export class UiLogin extends Component {
             console.log("msgId：",   msgId)
             switch (msgId)
             {
-                case MsgId.LoginRet:
+                case MsgId.AddRoleRet:
                     {
-                        console.log('登录成功');
+                        console.log('登录成功')
                         let id:number = arr[1]
                         let nickName:string = arr[2]
                         let prefabName:string = arr[3]
-                        console.log(id, nickName, prefabName, '进来了');
+                        console.log(id, nickName, prefabName, '进来了')
                         let old = entities.get(id)
                         if (old == undefined) {
-                            old = new ClientEntityComponent();
+                            old = new ClientEntityComponent()
                             entities.set(id, old)
                             resources.load(prefabName, Prefab, (err, prefab) => {
-                                console.log('resources.load callback:', err, prefab);
-                                const newNode = instantiate(prefab);
-                                roles.addChild(newNode);
-                                entityId[newNode.uuid]=id;
-                                //newNode.position = new Vec3(posX, 0, 0);
-                                console.log('resources.load newNode', newNode);
-                                old.view = newNode;
+                                console.log('resources.load callback:', err, prefab)
+                                const newNode = instantiate(prefab)
+                                roles.addChild(newNode)
+                                entityId[newNode.uuid]=id
+                                //newNode.position = new Vec3(posX, 0, 0)
+                                console.log('resources.load newNode', newNode)
+                                old.view = newNode
                                 old.skeletalAnimation = newNode.getComponent(SkeletalAnimation)
                                 old.skeletalAnimation.play(old.initClipName)
-                                let nodeCanvas = utils.find("Canvas", roles.parent);
-                                let nodeRoleName = utils.find("RoleName", nodeCanvas);
-                                // console.log('RoleName',this.nodeRoleName);
-                                // this.nodeRoleName.getComponent(HeadScale).target = this.nodeRoleName;
+                                let nodeCanvas = utils.find("Canvas", roles.parent)
+                                let nodeRoleName = utils.find("RoleName", nodeCanvas)
+                                // console.log('RoleName',this.nodeRoleName)
+                                // this.nodeRoleName.getComponent(HeadScale).target = this.nodeRoleName
 
-                                old.nodeName = instantiate(nodeRoleName);
+                                old.nodeName = instantiate(nodeRoleName)
                                 nodeCanvas.addChild(old.nodeName)
-                                old.labelName = old.nodeName.getComponent(Label);
+                                old.labelName = old.nodeName.getComponent(Label)
                                 let headScal = old.nodeName.getComponent(HeadScale)
-                                headScal.target = utils.find("RootNode/NamePos",newNode);
+                                headScal.target = utils.find("RootNode/NamePos",newNode)
                                 let camera3D = utils.find("Main Camera", roles.parent).getComponent(Camera)
                                 console.log('Main Camera',camera3D)
                                 //  headScal.camera = camera3D
                                 // headScal.distance = 55
                                 old.labelName.string = nickName +'('+ id + ')'
                                 old.nickName=nickName
-                                old.view.position = old.position;
-                            });
+                                if(old.position!=undefined)
+                                    old.view.position = old.position
+                            })
                         }
                         else {
-                            console.error('重复进入',id);
+                            console.error('重复进入',id)
                         }
                     }
-                    break;
+                    break
                 case MsgId.NotifyPos:
                     {
                         let id = arr[1]
@@ -224,35 +226,35 @@ export class UiLogin extends Component {
 
                         let old = entities.get(id)
                         if (old == undefined) {
-                        //    old = entites[id] = new ClientEntityComponent();
+                        //    old = entites[id] = new ClientEntityComponent()
                         //    resources.load("altman-blue", Prefab, (err, prefab) => {
-                        //        console.log('resources.load callback:', err, prefab);
-                        //        const newNode = instantiate(prefab);
-                        //        roles.addChild(newNode);
-                        //        newNode.position = new Vec3(posX, 0, 0);
-                        //        console.log('resources.load newNode', newNode);
-                        //        old.view = newNode;
+                        //        console.log('resources.load callback:', err, prefab)
+                        //        const newNode = instantiate(prefab)
+                        //        roles.addChild(newNode)
+                        //        newNode.position = new Vec3(posX, 0, 0)
+                        //        console.log('resources.load newNode', newNode)
+                        //        old.view = newNode
                         //        old.skeletalAnimation = newNode.getComponent(SkeletalAnimation)
                         //        old.skeletalAnimation.play(old.initClipName )
-                        //    });
+                        //    })
                         }
                         else {
                             if (old != undefined )
                             {
                                 // old.skeletalAnimation.play('run')
-                                old.position = new Vec3(posX, 0, posZ);
+                                old.position = new Vec3(posX, 0, posZ)
                                 old.hp = hp
                                 //console.log('angle',old.view.angle)
                             }
                             if( old.view != undefined)
                             {
-                                old.view.position = old.position;
+                                old.view.position = old.position
                                 old.view.eulerAngles = new Vec3(0,eulerAnglesY,0)
                                 old.labelName.string = old.nickName +'('+ id + ')hp=' + hp
                             }
                         }
                     }
-                    break;
+                    break
                 case MsgId.ChangeSkeleAnim:
                     {
                         let id = arr[1]
@@ -274,14 +276,26 @@ export class UiLogin extends Component {
                             old.skeletalAnimation.getState(clipName).wrapMode = loop ? AnimationClip.WrapMode.Loop : AnimationClip.WrapMode.Normal
                         }
                     }
-                    break;
+                    break
                 case MsgId.Say:
                     {
                         let content = arr[1]
                         console.log('有人说:', content)
                         lableMessage.string = content
                     }
+                    break
+                case MsgId.DelRoleRet:
+                    {
+                        let id = arr[1]
+                        console.log('删除:', id)
+                        let old = entities.get(id)
+                        old.view.removeFromParent()
+                        entities.delete(id)
+                    }
                     break;
+                default:
+                    console.error('msgId=',msgId)
+                    break
             }
         }
 
@@ -293,28 +307,28 @@ export class UiLogin extends Component {
 
         //监听窗口关闭事件，当窗口关闭时，主动去关闭websocket连接，防止连接还没断开就关闭窗口，server端会抛异常。
         window.onbeforeunload = function () {
-            console.log("onbeforeunload");
+            console.log("onbeforeunload")
         }
     }
     onClickSay(event: Event, customEventData: string) {
-        const node = event.target as Node;
-        const button = node.getComponent(Button);
-        const editNode = utils.find("Name", this.node) as Node;
-        console.log(button);
-        console.log(editNode);
+        const node = event.target as Node
+        const button = node.getComponent(Button)
+        const editNode = utils.find("Name", this.node) as Node
+        console.log(button)
+        console.log(editNode)
 
-        const editBox = editNode.getComponent(EditBox);
-        console.log(editBox.string);
-        const object = //item.hitPoint;
+        const editBox = editNode.getComponent(EditBox)
+        console.log(editBox.string)
+        const object = //item.hitPoint
         [
             MsgId.Say,
             editBox.string
-        ];
+        ]
 
-        const encoded: Uint8Array = msgpack.encode(object);
+        const encoded: Uint8Array = msgpack.encode(object)
         if (this.websocket != undefined) {
-            console.log('send',encoded);
-            this.websocket.send(encoded);
+            console.log('send',encoded)
+            this.websocket.send(encoded)
         }
     }
 }
