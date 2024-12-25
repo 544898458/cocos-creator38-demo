51972706633e8a0a18757a5c1927fa30d1b28a97
spjs <20024675@qq.com>
2024-12-25 21:55:40 +0800

    小程序还是连不上wss

---
+++ assets/Start.ts
@@ -0,0 +1,17 @@
+import { _decorator, Component, assetManager, AssetManager, director } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('Start')
+export class Start extends Component {
+    start() {
+        assetManager.loadBundle("scene",(err,bundle:AssetManager.Bundle)=>{
+            director.loadScene("scene登录");
+        });
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
+
