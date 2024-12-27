721b55eab8be21de6d53691d42efc002ce61bf36
spjs <20024675@qq.com>
2024-12-27 23:46:12 +0800

    用不同WS

--- assets/scripts/mode/UiLogin.ts
+++ assets/scripts/mode/UiLogin.ts
@@ -198,11 +198,11 @@ export class UiLogin extends Component {
         this.scene登录.nodeLoginPanel.active = false//隐藏
         this.scene登录.lableMessage.string = '正在连接'
         // this.websocket = new WebSocket("ws://192.168.31.194:12348/")
-        // this.websocket = new WebSocket("wss://192.168.31.170/")
+        // this.websocket = new WebSocket("ws://192.168.31.170:12348/")
         // this.websocket = new WebSocket("ws://192.168.43.109:12348/")
         // this.websocket = new WebSocket("ws://10.0.35.76:12345/")
         // this.websocket = new WebSocket("ws://192.168.0.100:12348/")
-        // this.websocket = new WebSocket("ws://47.119.184.177:12348/")
+        this.websocket = new WebSocket("ws://47.119.184.177:12348/")
         // this.websocket = new WebSocket("wss://wss.iotlabor.cn/")
         // We should pass the cacert to libwebsockets used in native platform, otherwise the wss connection would be closed.
         // let url = this.wssCacert.nativeUrl;
@@ -213,7 +213,7 @@ export class UiLogin extends Component {
         // this._wsiSendBinary = new WebSocket('wss://echo.websocket.events', [], url);
         // console.log(url)
         
-        this.websocket = new WebSocket('wss://wss2.iotlabor.cn:12348/',[])//, url)
+        // this.websocket = new WebSocket('wss://wss2.iotlabor.cn:12348/',[])//, url)
         // this.websocket = new WebSocket('wss://wss.iotlabor.cn/',[], url)
         // this.websocket = new WebSocket('wss://echo.websocket.events', [])//, url);
         this.websocket.binaryType = 'arraybuffer'
