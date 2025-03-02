e74ee63503577ff0a78086f0045763864f6f78e3
spjs <20024675@qq.com>
2025-03-02 21:39:26 +0800

    缩小贴图

--- assets/scripts/mode/Main.ts
+++ assets/scripts/mode/Main.ts
@@ -516,7 +516,7 @@ export class Main extends Component {
                 [MsgId.Login, ++this.sendMsgSn, 0, 0],
                 editBox.string,
                 'Hello, world!pwd',
-                3,//版本号
+                5,//版本号
             ]
 
             const encoded = msgpack.encode(object)
