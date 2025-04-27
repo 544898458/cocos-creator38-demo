7fce58c8b3d920c181cbd5d396bc55989e37fcb1
spjs <20024675@qq.com>
2025-04-28 00:06:54 +0800

    绿色坦克升级攻击速度

--- assets/scripts/component/按下按钮显示属性详情Component.ts
+++ assets/scripts/component/按下按钮显示属性详情Component.ts
@@ -25,12 +25,18 @@ export class 按下按钮显示属性详情Component extends 按下按钮显示�
 
 
         let 当前加数值 = this.battleUI.main.配置.find单位属性等级加数值(this.enum单位, this.enum属性, 当前属性等级) ?? 0
-        let str: string = '当前:\n' + 属性名称 + ': +' + 当前加数值 + '(' + 当前属性等级 + '级)'
+        const 当前加数值WithSign = 当前加数值 >= 0 ? `+${当前加数值}` : `${当前加数值}`;
+        let str: string = '当前:\n' + 属性名称 + ': ' + 当前加数值WithSign + '(' + 当前属性等级 + '级)'
 
         let 升级后等级 = 当前属性等级 + 1
         let 升级后加数值 = this.battleUI.main.配置.find单位属性等级加数值(this.enum单位, this.enum属性, 升级后等级)
-        if (升级后加数值) 
-            str += '\n\n升级后:\n' + 属性名称 + ': +' + 升级后加数值 + '(' + 升级后等级 + '级)'
+        if (升级后加数值) {
+            let 升级后加数值WithSign = 升级后加数值 >= 0 ? `+${升级后加数值}` : `${升级后加数值}`;
+            if( 属性类型.攻击前摇_伤害耗时 == this.enum属性)
+                升级后加数值WithSign += '毫秒'
+
+            str += '\n\n升级后:\n' + 属性名称 + ': ' + 升级后加数值WithSign + '(' + 升级后等级 + '级)'
+        }
         else
             str +='\n\n已满级'
 
