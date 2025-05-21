5e5203e25acd3b2264d07b80e7e25f22a7f6c7aa
liuyong <3380125833@qq.com>
2025-05-21 14:22:00 +0800

    代码报错

--- assets/scripts/component/按下按钮显示属性详情Component.ts
+++ assets/scripts/component/按下按钮显示属性详情Component.ts
@@ -25,12 +25,12 @@ export class 按下按钮显示属性详情Component extends 按下按钮显示�
 
 
 
-        let 当前加数值 = MainTest.instance.scene战斗.battleUI.main.配置.find单位属性等级加数值(this.enum单位, this.enum属性, 当前属性等级) ?? 0
+        let 当前加数值 = MainTest.instance.配置.find单位属性等级加数值(this.enum单位, this.enum属性, 当前属性等级) ?? 0
         const 当前加数值WithSign = 当前加数值 >= 0 ? `+${当前加数值}` : `${当前加数值}`;
         let str: string = '当前:\n' + 属性名称 + ': ' + 当前加数值WithSign + '(' + 当前属性等级 + '级)'
 
         let 升级后等级 = 当前属性等级 + 1
-        let 升级后加数值 = MainTest.instance.scene战斗.battleUI.main.配置.find单位属性等级加数值(this.enum单位, this.enum属性, 升级后等级)
+        let 升级后加数值 = MainTest.instance.配置.find单位属性等级加数值(this.enum单位, this.enum属性, 升级后等级)
         if (升级后加数值) {
             let 升级后加数值WithSign = 升级后加数值 >= 0 ? `+${升级后加数值}` : `${升级后加数值}`;
             if (属性类型.攻击前摇_伤害耗时 == this.enum属性)
