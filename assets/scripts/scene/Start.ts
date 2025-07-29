db2aab3211a73e93a06ace72502e219157c11c4e
spjs <544898458@qq.com>
2025-07-30 03:19:50 +0800

    简单测试抖音小游戏，远程包

--- assets/scripts/scene/Start.ts
+++ assets/scripts/scene/Start.ts
@@ -1,11 +1,17 @@
 import { _decorator, Component, assetManager, AssetManager, director } from 'cc';
+import { MainTest } from '../MainTest';
 const { ccclass, property } = _decorator;
 
 @ccclass('Start')
 export class Start extends Component {
     start() {
-        assetManager.loadBundle("scene",(err,bundle:AssetManager.Bundle)=>{
-            // director.loadScene("scene登录");
+        let strUri = "resources"
+        if(MainTest.是抖音小游戏()){
+            strUri = "https://www.rtsgame.online/bytedance/remote/resources"
+        }
+        
+        assetManager.loadBundle(strUri,(err,bundle:AssetManager.Bundle)=>{
+            console.log(err, bundle);
             director.loadScene("main");
         });
     }
