d7efc803707786645c68e351d7a8a8aee2449d38
spjs <544898458@qq.com>
2025-10-25 12:26:01 +0800

    反防空战，虫族

--- assets/scripts/配置/配置.ts
+++ assets/scripts/配置/配置.ts
@@ -81,8 +81,8 @@ export class 配置 {
 	}
 	读取1个配置文件<T>(strName: string, fun: (arr: Array<T>) => void) {
 
-		let url = 'https://www.rtsgame.online/配置/'	//改兵营模型	版本35
-		// let url = 'https://www.rtsgame.online/配置2/'	//改机场模型
+		// let url = 'https://www.rtsgame.online/配置/'	//改兵营模型	版本35
+		let url = 'https://www.rtsgame.online/配置2/'	//虫族反防空战	版本36
 		assetManager.loadRemote(encodeURI(url + strName + '.yaml'), { ext: '.txt' },
 			(err, textAsset: TextAsset) => {
 				console.log(err, textAsset)
