1f5c7842c9d9a3e6d49ebdcda835dc6221bfd5df
spjs <544898458@qq.com>
2026-01-27 02:18:14 +0800

    英语音效方案

--- assets/scripts/component/按下按钮显示文本详情Component.ts
+++ assets/scripts/component/按下按钮显示文本详情Component.ts
@@ -1,17 +1,17 @@
 
 import { _decorator, Component, Node } from 'cc';
-import { 单位类型, 配置, 单位配置 } from '../配置/配置';
+import { 配置, 单位配置 } from '../配置/配置';
 import { 按下按钮显示详情Component } from './按下按钮显示详情Component';
 import { CCString } from 'cc';
+import { 翻译Key } from '../配置/翻译Key';
 const { ccclass, property } = _decorator;
 
 @ccclass('按下按钮显示文本详情')
 export class 按下按钮显示文本详情Component extends 按下按钮显示详情Component {
     @property({ type: CCString })
-    str详情: String
+    str详情Key: string
 
-    详情(): String {
-
-        return this.str详情
+    详情(): string {
+        return 翻译Key.翻译(this.str详情Key)
     }
 }
