fcc0ca4ece1906166ede555154949d0ed4753351
spjs <544898458@qq.com>
2026-09-14 21:38:16 +0800

    准备测试创建机甲

--- assets/scripts/配置/此游戏专用配置.ts
+++ assets/scripts/配置/此游戏专用配置.ts
@@ -1,4 +1,4 @@
 export const host静态 = 'https://cdn.rtsgame.online/'
-// const url配置 = host静态 + '即时战略指挥配置1/' //20260328 打电脑   版本号50
-const url配置 = host静态 + '即时战略指挥配置2/' //20260623 改地堡模型  版本号51
+const url配置 = host静态 + '即时战略指挥配置1/' //20260915 机甲   版本号52
+// const url配置 = host静态 + '即时战略指挥配置2/' //20260623 改地堡模型  版本号51
 export default url配置
